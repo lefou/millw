55998337539921287897ca02f1e59b95896bfb92
Tobias Roeser <le.petit.fou@web.de>
2022-02-15 23:50:51 +0100

    Prepared release 0.4.2

--- millw.bat
+++ millw.bat
@@ -5,7 +5,7 @@ rem You can give the required mill version with --mill-version parameter
 rem If no version is given, it falls back to the value of DEFAULT_MILL_VERSION
 rem
 rem Project page: https://github.com/lefou/millw
-rem Script Version: 0.4.1
+rem Script Version: 0.4.2
 rem
 rem If you want to improve this script, please also contribute your changes back!
 rem
