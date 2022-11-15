53ddc046861aa84745c58072d29ae58de1b6d8d7
Tobias Roeser <le.petit.fou@web.de>
2022-11-15 11:05:41 +0100

    .

--- millw.bat
+++ millw.bat
@@ -122,7 +122,6 @@ rem Need to preserve the first position of those listed options
 set MILL_FIRST_ARG=
 if [%~1%]==[--bsp] (
   set MILL_FIRST_ARG=%1%
-  shift
 ) else (
   if [%~1%]==[-i] (
     set MILL_FIRST_ARG=%1%
@@ -145,8 +144,27 @@ if [%~1%]==[--bsp] (
   )
 )
 
-if [!MILL_FIRST_ARG!]==[] goto :AfterStripFirstArg
-shift
-:AfterStripFirstArg
+set "MILL_PARAMS=%*%"
 
-"%MILL%" %MILL_FIRST_ARG% -D "mill.main.cli=%MILL_MAIN_CLI%" %*%
+if not [!MILL_FIRST_ARG!]==[] (
+  if defined STRIP_VERSION_PARAMS (
+    for /f "tokens=1-3*" %%a in ("%*") do (
+        set "MILL_PARAMS=%%d"
+    )
+  ) else (
+    for /f "tokens=1*" %%a in ("%*") do (
+      set "MILL_PARAMS=%%b"
+    )
+  )
+) else (
+  if defined STRIP_VERSION_PARAMS (
+    for /f "tokens=1-2*" %%a in ("%*") do (
+        rem strip %%a - It's the "--mill-version" option.
+        rem strip %%b - it's the version number that comes after the option.
+        rem keep  %%c - It's the remaining options.
+        set "MILL_PARAMS=%%c"
+    )
+  )
+)
+
+"%MILL%" %MILL_FIRST_ARG% -D "mill.main.cli=%MILL_MAIN_CLI%" %MILL_PARAMS%
